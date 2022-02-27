23cdf9a12e785650ab934f4d9e01015a8ba9ba2d
Eduardo Lima <du.lima1337@gmail.com>
2022-02-27 14:06:44 -0300

    Fixed navigation component

---
+++ frontend/src/components/Navigation/Navigation.styles.ts
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+    margin-bottom: 5rem;
+    background: var(--gray-50);
+`
+
+export const Content = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+    max-width: 1120px;
+    margin: 0 auto;
+    padding: 2rem 1rem 2rem;
+    color: var(--gray-900);
+    .title {
+        font-size: 3rem;
+        font-weight: 600;
+    }
+    button {
+        width: 3rem;
+    }
+`;
---
+++ frontend/src/components/Navigation/Navigation.tsx
@@ -0,0 +1,27 @@
+/**
+ * Header Component
+ */
+
+ import { useContext } from 'react';
+ import { RiShoppingCart2Line } from 'react-icons/ri';
+ 
+ import { CartContext } from '../../CartContext';
+ import { Button } from '../Button/Button';
+ 
+ import { Container, Content } from './Navigation.styles';
+ 
+ export const Navigation = () => {
+   const { isOpen, setIsOpen } = useContext(CartContext);
+ 
+   const toggleCart = () => setIsOpen(!isOpen);
+ 
+   return (
+     <Container>
+       <Content>
+         <span className="title">Bonsai.</span>
+         <Button onClick={toggleCart}><RiShoppingCart2Line /></Button>
+       </Content>
+     </Container>
+   );
+ };
+ 
